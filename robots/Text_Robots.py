02711c971fc42b0313230804252a4c4191547c4f
Raphael Nascimento <raphael_nask@hotmail.com>
2019-08-26 01:36:50 -0300

    Implementando busca no wiki

---
+++ robots/Text_Robots.py
@@ -0,0 +1,8 @@
+class TextRobots(object):
+    def __init__(self, content= []):
+        self.conteudoWikipedia()
+        #self.sanitizarCOnteudo()
+        #self.quebrarSentencas()
+
+    def conteudoWikipedia(self):
+        #...
