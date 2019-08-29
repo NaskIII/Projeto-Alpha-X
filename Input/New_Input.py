9147501c2b6b9a1167c08a05e15fb4b8f9409d60
Raphael Nascimento <raphael_nask@hotmail.com>
2019-08-29 01:03:54 -0300

    O código foi comentado para melhores reescritas e implementações

--- Input/New_Input.py
+++ Input/New_Input.py
@@ -1,30 +1,30 @@
-from robots import New_Text_Robots
+from robots import New_Text_Robots  # Importo o robo responsavel pelo texto
 
 
 def start():
-    def inputTermo():
+    def inputTermo():  # Aqui eu pego um termo para ser pesquisado
         print()
         termo = input('Digite um termo para o Wikipedia: ')
         print()
         return termo
 
-    def inputPrefixo():
+    def inputPrefixo():  # Aqui recebo um prefixo, pois a maquina so sabe o termo, nao o objetivo dele
         prefixos = ['Quem e', 'O que e', 'A historia', 'Exit', '']
         print ('Escolha um:')
-        for index, item in enumerate(prefixos):
-            print(index + 1, item)
+        for index, item in enumerate(prefixos):  # Itero minha lista para enumerar
+            print(index + 1, item)  # Escrevo o conteudo com uma soma para nao aparecer o valor 0
 
         print()
         escolha = int(input('>> '))
-        return prefixos[escolha-1]
+        return prefixos[escolha-1]  # Retorno como subtraçao pois somei logo acima
 
-    artigo = {
+    artigo = {  # Dict que e passada como parametro para o robo de texto
         'termo': inputTermo(),
         'prefixo': inputPrefixo()
     }
 
-    def call():
-        robotText = New_Text_Robots.TextRobots(artigo, '/home/nask/Documentos/Arquivos/').resumir()
+    def call():  # Metodo responsavel por chamar todos os robos
+        robotText = New_Text_Robots.TextRobots(artigo, '/home/nask/Documentos/Arquivos/').resumir()  # Construtor com chamada do metodo
 
     call()
 start()
