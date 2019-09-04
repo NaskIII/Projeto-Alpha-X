04046e84ae27221290955e100d80292fac53dc54
Raphael Nascimento <raphael_nask@hotmail.com>
2019-09-04 15:53:04 -0300

    A Aplicação está funcionando 100% no sistema Linux e está finalizada

--- Old_robots/Word.py
+++ Old_robots/Word.py
@@ -4,18 +4,18 @@ ID: Nask
 Objetivo: Pegar o Texto formatado de um .txt e reescreve-lo no formato docx
 '''
 
-
 import docx
 
 
 class Docx(object):
-    def __init__(self, caminho, content):
+    def __init__(self, caminho, diretorio, content):
         self.caminho = caminho
+        self.diretorio = diretorio
         self.content = content
         self.lista = []
 
     def contLines(self):  # Itero o arquivo para poder saber o numero de linhas
-        arq = open(self.caminho , 'r')
+        arq = open(self.caminho, 'r')
         linhas = len(arq.readlines())
         arq.close()
         return linhas
@@ -32,23 +32,24 @@ class Docx(object):
         documento = docx.Document()
         style = documento.styles['Normal']
         font = style.font
-        font.name = 'Arimo'
-        font.size = docx.shared.Pt(15)
+        font.name = 'Arial'
+        font.size = docx.shared.Pt(11)
+
         documento.add_heading(self.content['title'], 0)
 
-        p = documento.add_paragraph(self.lista[0])
-
         for i in self.lista:
-            if len(i) <= 20:
+            if 7 >= len(i) > 0:
+                documento.add_heading(i, level=0)
+            elif 10 >= len(i) > 0:
                 documento.add_heading(i, level=1)
-            elif len(i) <= 30:
+            elif 20 >= len(i) > 0:
                 documento.add_heading(i, level=2)
+            elif 35 >= len(i) > 0:
+                documento.add_heading(i, level=3)
             else:
                 documento.add_paragraph(i)
-
-        documento.save(self.content['title'] + '.docx')
+        documento.save(self.diretorio + '/' + self.content['title'] + '.docx')
 
     def chamadas(self):
         self.read()
         self.docx()
-
