004d22df71e304c72f046edad973defa2ea611b4
Raphael Nascimento <raphael_nask@hotmail.com>
2019-09-04 16:22:27 -0300

    A Aplicação agora funciona em sistemas Windows

--- robots/Diretorios.py
+++ robots/Diretorios.py
@@ -6,6 +6,7 @@ Objetivo: Criar pastas para os arquivos serem criadaos
 
 import os.path  # Modulo usado para manipular os caminhos
 import platform  # Modulo usado para identificar o SO
+import sys
 
 
 def start(artigo):  # Metodo que recebe uma dict, usado para nomear as pastas
@@ -14,13 +15,17 @@ def start(artigo):  # Metodo que recebe uma dict, usado para nomear as pastas
         so = platform.system()
         if so == 'Linux':
             dir = os.path.expanduser('~/Documentos/')  # Pego o caminho ate a pasta Documentos
-            os.makedirs(dir + 'Arquivos/' + artigo['termo'] + '/',
-                        exist_ok=True)  # Crio a pasta Arquivos e outra com o titulo da busca, se existir nao me retorna nenhum exeçao
-            dir = os.path.expanduser('~/Documentos/Arquivos/' + artigo[
-                'termo'] + '/')  # Pego o caminho completo para ser retornado ao robo de texto
+            os.makedirs(dir + 'Arquivos/' + artigo['termo'] + '/', exist_ok=True)  # Crio a pasta Arquivos e outra com o titulo da busca, se existir nao me retorna nenhum exeçao
+            dir = os.path.expanduser('~/Documentos/Arquivos/' + artigo['termo'] + '/')  # Pego o caminho completo para ser retornado ao robo de texto
             return dir
         elif so == 'Windows':
-            print("...")
+            dir = os.path.expanduser('~/Documentos')
+            os.makedirs(dir + 'WikiDocs/' + artigo['termo'] + '/', exist_ok=True)
+            dir = os.path.expanduser('/Documentos/Arquivos/' + artigo['termo'] + '/')
+            return dir
+        else:
+            print('Sistema operacional não identificado')
+            sys.exit()
 
     dir = diretorios()
 
